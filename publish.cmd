a08c8a0118e5ba77c3a9c71625bfe33e6ca1d0e5
Noure <noureddinechaouch@MacBook-Pro-de-Noureddine.local>
2018-05-29 14:49:01 +0200

    Start

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=migrationbot-Web-Deploy.pubxml -p:Password=1mhtQtRY5qoiud96E4Sm1WQrKTdu8T7qq4sBGd1ZlXlnkefuLNMmzcsPC0om
+
